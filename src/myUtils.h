6d4f2b1252d2f751cac081078ebf2713e7ae8215
sbecht <sbecht>
2018-11-01 13:00:24 +0100

    Moved implementation code from myUtils.h to new file myUtils.cpp

--- src/myUtils.h
+++ src/myUtils.h
@@ -1,16 +1,14 @@
+
+#ifndef MYUTILS_H_
+#define MYUTILS_H_
+
 #include <vector>
 #include <string>
 #include <sstream>
 
-std::vector<std::string> splitString(std::string str, char delimiter) {
+std::vector<std::string> splitString(std::string str, char delimiter);
 
-	std::vector<std::string> internal;
-	std::stringstream ss(str); // Turn the string into a stream.
-	std::string tok;
 
-	while (getline(ss, tok, delimiter)) {
-		internal.push_back(tok);
-	}
+std::istream& safeGetline(std::istream& is, std::string& t);
 
-	return internal;
-}
+#endif /* MYUTILS_H_ */
